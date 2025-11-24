db7b1d132841c7a39feed41d6bee1f863f5eabd6
Pavan.Kumar <pavanln049@gmail.com>
2025-11-24 20:41:17 +0100

    getting all the types of training datasets

--- notebooks/Modis_data_analysis/torch_data_loader.py
+++ notebooks/Modis_data_analysis/torch_data_loader.py
@@ -28,7 +28,7 @@ from notebooks.Modis_data_analysis.PM25_data_loader_analysis import Training_dat
 # config["output_vars"]# defining the output variable here.
 
 
-class AirQualityDataset(Dataset):
+class AirQualityDataset_latlon_AOD_PM25(Dataset):
     # here flag will be default Train but if we assign something new it will consider that
     # here the df will be the df coming out from our final trainig data from PM25_data_loader_analysis file
 
@@ -55,6 +55,260 @@ class AirQualityDataset(Dataset):
 
         # these are the input columns for the model
         self.input_cols = self.config["experiments"]["input_vars"][input_type_1]
+        # these are the targets
+        self.target_cols = self.config["output_vars"]
+
+        # here we will select the range of the train, test and validation of data sets.
+        # select date range
+        if flag == "Train":
+            a, b = "train_start", "train_end"
+        elif flag == "Val":
+            a, b = "val_start", "val_end"
+        elif flag == "Test":
+            a, b = "test_start", "test_end"
+        else:
+            raise ValueError("flag is not matching with Train, Val, Test")
+        # let's define the functions
+        self.start_time = self._get_time(ds_cfg[a])
+        self.end_time = self._get_time(ds_cfg[b])
+        self.df["date"] = pd.to_datetime(self.df["date"])
+        self.data = self.df[
+            (self.df["date"] >= self.start_time) & (self.df["date"] <= self.end_time)
+        ]
+
+    def _get_time(self, time_yaml):
+        # in this line the data set that will be passed will be
+        arrow_time = arrow.get(datetime(*time_yaml[0]), time_yaml[1]).naive
+        return arrow_time
+
+    # get the length of the data-set
+    def __len__(self):
+        return len(self.data)
+
+    def print_data_type(self, idx):
+        data_rows = self.data.iloc[idx]
+        print(data_rows[self.input_cols].values)
+        print(data_rows[self.target_cols].values)
+
+    # get the items train as well as test from the data-set
+    def __getitem__(self, idx):
+        data_rows = self.data.iloc[idx]
+
+        x = torch.tensor(
+            # all the numeric values should match the data type all should be similer data type
+            pd.to_numeric(data_rows[self.input_cols], errors="coerce")
+            .astype(np.float32)
+            .values,
+            dtype=torch.float32,
+        )
+        # all the numeric values should match the data type all should be similer data type
+        y = torch.tensor(
+            pd.to_numeric(data_rows[self.target_cols], errors="coerce")
+            .astype(np.float32)
+            .values,
+            dtype=torch.float32,
+        )
+        return x, y
+
+
+class AirQualityDataset_latlon_AOD(Dataset):
+    # here flag will be default Train but if we assign something new it will consider that
+    # here the df will be the df coming out from our final trainig data from PM25_data_loader_analysis file
+
+    def __init__(self, df, config, flag="Train"):
+        # here we will store them as attributes.
+        if df is None or config is None:
+            raise ValueError("put the input data set and config file also")
+        self.df = df
+        self.config = config
+        self.flag = flag
+
+        # Here as we are taking the data from config file the number of data sets it will make our code to make it moduler to also accept more than one data set if we have in future.
+
+        self.dataset_number = self.config["experiments"]["dataset_num_1"]
+
+        # This we use to extract the training, test and validation data sets dates in the given config file.
+
+        ds_cfg = self.config["dataset"][self.dataset_number]
+        # Select input and target in such as way that we can change it later to do the changes easily.
+
+        # here we have selected the input type = 1
+
+        input_type_2 = self.config["experiments"]["input_type_2"]
+
+        # these are the input columns for the model
+        self.input_cols = self.config["experiments"]["input_vars"][input_type_2]
+        # these are the targets
+        self.target_cols = self.config["output_vars"]
+
+        # here we will select the range of the train, test and validation of data sets.
+        # select date range
+        if flag == "Train":
+            a, b = "train_start", "train_end"
+        elif flag == "Val":
+            a, b = "val_start", "val_end"
+        elif flag == "Test":
+            a, b = "test_start", "test_end"
+        else:
+            raise ValueError("flag is not matching with Train, Val, Test")
+        # let's define the functions
+        self.start_time = self._get_time(ds_cfg[a])
+        self.end_time = self._get_time(ds_cfg[b])
+        self.df["date"] = pd.to_datetime(self.df["date"])
+        self.data = self.df[
+            (self.df["date"] >= self.start_time) & (self.df["date"] <= self.end_time)
+        ]
+
+    def _get_time(self, time_yaml):
+        # in this line the data set that will be passed will be
+        arrow_time = arrow.get(datetime(*time_yaml[0]), time_yaml[1]).naive
+        return arrow_time
+
+    # get the length of the data-set
+    def __len__(self):
+        return len(self.data)
+
+    def print_data_type(self, idx):
+        data_rows = self.data.iloc[idx]
+        print(data_rows[self.input_cols].values)
+        print(data_rows[self.target_cols].values)
+
+    # get the items train as well as test from the data-set
+    def __getitem__(self, idx):
+        data_rows = self.data.iloc[idx]
+
+        x = torch.tensor(
+            # all the numeric values should match the data type all should be similer data type
+            pd.to_numeric(data_rows[self.input_cols], errors="coerce")
+            .astype(np.float32)
+            .values,
+            dtype=torch.float32,
+        )
+        # all the numeric values should match the data type all should be similer data type
+        y = torch.tensor(
+            pd.to_numeric(data_rows[self.target_cols], errors="coerce")
+            .astype(np.float32)
+            .values,
+            dtype=torch.float32,
+        )
+        return x, y
+
+
+# -------------************---------------------------------
+# Data set with inputs latitude, longitude and AOD values as inputs.
+# Data set with AOD and PM2.5 values as inputs.
+
+
+class AirQualityDataset_latlon_PM25(Dataset):
+    # here flag will be default Train but if we assign something new it will consider that
+    # here the df will be the df coming out from our final trainig data from PM25_data_loader_analysis file
+
+    def __init__(self, df, config, flag="Train"):
+        # here we will store them as attributes.
+        if df is None or config is None:
+            raise ValueError("put the input data set and config file also")
+        self.df = df
+        self.config = config
+        self.flag = flag
+
+        # Here as we are taking the data from config file the number of data sets it will make our code to make it moduler to also accept more than one data set if we have in future.
+
+        self.dataset_number = self.config["experiments"]["dataset_num_1"]
+
+        # This we use to extract the training, test and validation data sets dates in the given config file.
+
+        ds_cfg = self.config["dataset"][self.dataset_number]
+        # Select input and target in such as way that we can change it later to do the changes easily.
+
+        # here we have selected the input type = 1
+
+        input_type_3 = self.config["experiments"]["input_type_3"]
+
+        # these are the input columns for the model
+        self.input_cols = self.config["experiments"]["input_vars"][input_type_3]
+        # these are the targets
+        self.target_cols = self.config["output_vars"]
+
+        # here we will select the range of the train, test and validation of data sets.
+        # select date range
+        if flag == "Train":
+            a, b = "train_start", "train_end"
+        elif flag == "Val":
+            a, b = "val_start", "val_end"
+        elif flag == "Test":
+            a, b = "test_start", "test_end"
+        else:
+            raise ValueError("flag is not matching with Train, Val, Test")
+        # let's define the functions
+        self.start_time = self._get_time(ds_cfg[a])
+        self.end_time = self._get_time(ds_cfg[b])
+        self.df["date"] = pd.to_datetime(self.df["date"])
+        self.data = self.df[
+            (self.df["date"] >= self.start_time) & (self.df["date"] <= self.end_time)
+        ]
+
+    def _get_time(self, time_yaml):
+        # in this line the data set that will be passed will be
+        arrow_time = arrow.get(datetime(*time_yaml[0]), time_yaml[1]).naive
+        return arrow_time
+
+    # get the length of the data-set
+    def __len__(self):
+        return len(self.data)
+
+    def print_data_type(self, idx):
+        data_rows = self.data.iloc[idx]
+        print(data_rows[self.input_cols].values)
+        print(data_rows[self.target_cols].values)
+
+    # get the items train as well as test from the data-set
+    def __getitem__(self, idx):
+        data_rows = self.data.iloc[idx]
+
+        x = torch.tensor(
+            # all the numeric values should match the data type all should be similer data type
+            pd.to_numeric(data_rows[self.input_cols], errors="coerce")
+            .astype(np.float32)
+            .values,
+            dtype=torch.float32,
+        )
+        # all the numeric values should match the data type all should be similer data type
+        y = torch.tensor(
+            pd.to_numeric(data_rows[self.target_cols], errors="coerce")
+            .astype(np.float32)
+            .values,
+            dtype=torch.float32,
+        )
+        return x, y
+
+
+class AirQualityDataset_latlon(Dataset):
+    # here flag will be default Train but if we assign something new it will consider that
+    # here the df will be the df coming out from our final trainig data from PM25_data_loader_analysis file
+
+    def __init__(self, df, config, flag="Train"):
+        # here we will store them as attributes.
+        if df is None or config is None:
+            raise ValueError("put the input data set and config file also")
+        self.df = df
+        self.config = config
+        self.flag = flag
+
+        # Here as we are taking the data from config file the number of data sets it will make our code to make it moduler to also accept more than one data set if we have in future.
+
+        self.dataset_number = self.config["experiments"]["dataset_num_1"]
+
+        # This we use to extract the training, test and validation data sets dates in the given config file.
+
+        ds_cfg = self.config["dataset"][self.dataset_number]
+        # Select input and target in such as way that we can change it later to do the changes easily.
+
+        # here we have selected the input type = 1
+
+        input_type_4 = self.config["experiments"]["input_type_4"]
+
+        # these are the input columns for the model
+        self.input_cols = self.config["experiments"]["input_vars"][input_type_4]
         # these are the targets
         self.target_cols = self.config["output_vars"]
 
---
+++ src/training/trainer_latlon_AOD_PM25.py
@@ -0,0 +1,112 @@
+import os
+import torch
+import numpy as np
+import pandas as pd
+import sys
+from pathlib import Path
+import os
+import matplotlib.pyplot as plt
+from glob import glob
+from tqdm import tqdm
+import arrow
+from datetime import datetime
+# from utils.config import config
+from torch.utils.data import DataLoader
+ROOT = Path(__file__).resolve().parents[2]  # tests/ -> project root
+sys.path.insert(0, str(ROOT))
+print(ROOT)
+from notebooks.Modis_data_analysis.utils import config
+# -------------************---------------------------------
+# loading all the data loaders here we will load the final data in torch.
+from notebooks.Modis_data_analysis.PM25_data_loader_analysis import Modis_data_loader
+from notebooks.Modis_data_analysis.PM25_data_loader_analysis import PM_25_dataloader
+from notebooks.Modis_data_analysis.PM25_data_loader_analysis import (
+    combine_the_data_frames,
+)
+from notebooks.Modis_data_analysis.PM25_data_loader_analysis import Training_data_loader
+from locationencoder.final_location_encoder import Geospatial_Encoder
+from notebooks.Modis_data_analysis.torch_data_loader import (
+    AirQualityDataset_latlon_AOD_PM25,
+    AirQualityDataset_latlon_AOD,
+    AirQualityDataset_latlon_PM25,
+    AirQualityDataset_latlon,
+)
+# importing config files
+from notebooks.Modis_data_analysis.final_loader import (
+    Final_Air_Quality_Dataset_pipeline_latlon_AOD_PM25,
+    Final_Air_Quality_Dataset_pipeline_latlon_AOD,
+    Final_Air_Quality_Dataset_pipeline_latlon_PM25,
+    Final_Air_Quality_Dataset_pipeline_latlon,
+)
+# -------------************--------------------------------
+# Creating all the instance here
+
+instance_latlon_AOD_PM25 = Final_Air_Quality_Dataset_pipeline_latlon_AOD_PM25(config)
+
+instance_latlon_AOD = Final_Air_Quality_Dataset_pipeline_latlon_AOD(config)
+
+instance_latlon_PM25 = Final_Air_Quality_Dataset_pipeline_latlon_PM25(config)
+
+intance_latlon = Final_Air_Quality_Dataset_pipeline_latlon(config)
+
+
+# final data with latitude, longitude, AOD and PM2.5
+instance_latlon_AOD_PM25.modis_data_sets()
+instance_latlon_AOD_PM25.stations_data_sets()
+instance_latlon_AOD_PM25.PM_25_data()
+instance_latlon_AOD_PM25.training_data()
+instance_latlon_AOD_PM25.Torch_data()
+final_data_latlon_AOD_PM25 = instance_latlon_AOD_PM25.full_pipeline()
+final_data_latlon_AOD_PM25[0][0].shape
+final_data_latlon_AOD_PM25[1][0]
+
+# final data with latitude, longitude and AOD
+
+instance_latlon_AOD.modis_data_sets()
+instance_latlon_AOD.stations_data_sets()
+instance_latlon_AOD.PM_25_data()
+instance_latlon_AOD.training_data()
+instance_latlon_AOD.Torch_data()
+final_data_latlong_AOD = instance_latlon_AOD.full_pipeline()
+final_data_latlong_AOD[0][0].shape
+final_data_latlong_AOD[1][0]
+
+# final data with latitude, longitude  and PM2.5
+instance_latlon_PM25.modis_data_sets()
+instance_latlon_PM25.stations_data_sets()
+instance_latlon_PM25.PM_25_data()
+instance_latlon_PM25.training_data()
+instance_latlon_PM25.Torch_data()
+final_data_latlong_PM25 = instance_latlon_PM25.full_pipeline()
+final_data_latlong_PM25[0][0].shape
+final_data_latlong_PM25[1][0]
+
+# final data with latitude and longitude
+intance_latlon.modis_data_sets()
+intance_latlon.stations_data_sets()
+intance_latlon.PM_25_data()
+intance_latlon.training_data()
+intance_latlon.Torch_data()
+final_data_latlong = intance_latlon.full_pipeline()
+final_data_latlong[0][0].shape
+final_data_latlong[1][0]
+
+
+# -------------************--------------------------------
+"""
+Final training data 
+"""
+
+
+final_data_latlon_AOD_PM25
+final_data_latlong_AOD
+final_data_latlong_PM25
+final_data_latlong
+
+
+# -------------************--------------------------------
+
+# training the model in different sdata sets and storing the weights.
+
+# -------------************--------------------------------
+
