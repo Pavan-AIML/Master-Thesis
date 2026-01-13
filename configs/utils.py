b50effa0223ecdd60317bd9ada5e095ba0866739
Pavan.Kumar <pavanln049@gmail.com>
2026-01-13 18:29:40 +0100

    lfs git

--- configs/utils.py
+++ configs/utils.py
@@ -4,14 +4,13 @@ import os
 import sys
 from pathlib import Path
 
-ROOT = Path(__file__).resolve().parents[2]  # tests/ -> project root
+ROOT = Path(__file__).resolve().parents[1]  # tests/ -> project root
 sys.path.insert(0, str(ROOT))
 print(ROOT)
 
 
-folder_path = (
-    "/Users/pavankumar/Documents/Winter_Thesis/Coding_Learning/Master-Thesis/configs"
-)
+folder_path = ROOT / "configs"
+folder_path
 yaml_path = os.path.join(folder_path, "config.yaml")
 
 with open(yaml_path, "r") as file:
